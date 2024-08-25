bc92362c85ab6c09c21a1d72c1a3c024154fd329
Alikin Hodkon <kakto228030904@gmail.com>
2024-08-25 16:23:42 +0300

    remove_ref

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { IDays, ITask } from "../types"
 import Task from "./Task";
 
@@ -11,7 +11,6 @@ interface DayProps{
 }
 export default function Day({day, tasks, addTask, deleteTask, changeTask}: DayProps) {
     const [task, setTask] = useState<string>("");
-    const inputRef= useRef();
   return (
     <div className="w-[15%] flex flex-col border-black border-[2px] min-h-[100vh]">
         <div className="text-[32px] border-b-2 border-black flex justify-center">
@@ -20,8 +19,8 @@ export default function Day({day, tasks, addTask, deleteTask, changeTask}: DayPr
             </div>
         {tasks.map((task) => <Task key={task.id} task={task} deleteTask={deleteTask} changeTask={changeTask} />)}
         <div className="flex w-full mt-2">
-            <input ref={inputRef} type="text" className="w-3/4 pl-2 rounded-xl border-black border-[2px]" onChange={(e) => setTask(e.target.value)} placeholder="Дело" />
-            <button className="w-1/3" onClick={() => {addTask(task, day.date, day.time); inputRef.current.value = ""}}>Добавить</button>
+            <input type="text" className="w-3/4 pl-2 rounded-xl border-black border-[2px]" onChange={(e) => setTask(e.target.value)} placeholder="Дело" />
+            <button className="w-1/3" onClick={() => {addTask(task, day.date, day.time);}}>Добавить</button>
         </div>
     </div>
   )
